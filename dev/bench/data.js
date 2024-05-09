f4a5da7278563d59318f8e2b6097108593b971be
github-action-benchmark <github@users.noreply.github.com>
2024-05-09 09:39:19 +0000

    add tidyt benchmarks (cargo) benchmark result for de11a6bd71ee48618c7104e5680b63ec77efe467

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1715247559833,
+  "repoUrl": "https://github.com/hackworthltd/tidyt",
+  "entries": {
+    "tidyt benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "src@drewhess.com",
+            "name": "Drew Hess",
+            "username": "dhess"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de11a6bd71ee48618c7104e5680b63ec77efe467",
+          "message": "chore(deps): bump num from 0.4.2 to 0.4.3 (#15)",
+          "timestamp": "2024-05-09T09:36:29Z",
+          "tree_id": "5d31ff800f78bcf99a35e493ecb891b132229e3a",
+          "url": "https://github.com/hackworthltd/tidyt/commit/de11a6bd71ee48618c7104e5680b63ec77efe467"
+        },
+        "date": 1715247559547,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tidy layout",
+            "value": 38298826,
+            "range": "± 1511171",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "tidy layout large",
+            "value": 548936902,
+            "range": "± 6846561",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "naive layout",
+            "value": 12084201,
+            "range": "± 721472",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
